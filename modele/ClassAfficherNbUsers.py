f9a5bd7e9c64395292aefb19c3831eaf4f4e5665
JUNIER <christophejunier@gmail.com>
2017-03-16 21:28:06 +0100

    commentaire sur le nbutilisateurs

--- modele/ClassAfficherNbUsers.py
+++ modele/ClassAfficherNbUsers.py
@@ -5,22 +5,25 @@ from datetime import datetime
 
 class afficherNbUsers:
 
+    # Notre méthode constructeur
     def _init_(self):
-        self.usersDictionnary = {}
-        self.usersLogTime = datetime.now()
+        self.usersDictionnary = {} #dictionnaire clef: valeur = badge: date
+        self.usersLogTime = datetime.now() #date de l'entrer ou de la sortie
 
+    # Notre méthode  qui genere et actualise la liste des utilisateurs present
     def GererUtilisateurListe(self,ID_badge):
-        if self.usersDictionnary.has_key(ID_badge):
-            del self.usersDictionnary[ID_badge]
-        else:
-            self.usersDictionnary[ID_badge] = self.usersLogTime
+        if self.usersDictionnary.has_key(ID_badge):  #si le badge est presente dans le dictionnaire
+            del self.usersDictionnary[ID_badge]         #on l'enlève
+        else:                                        #sinon
+            self.usersDictionnary[ID_badge] = self.usersLogTime #on l'ajoute
 
+    # Notre méthode  qui renvoie le nombre d'utilisateur
     def afficherNbUsers(self):
         return("Nombre de personne présente :" + len(liste_entrer))#retourne le nombre de personne présente
 
-
+    # Notre méthode enleve les utilisateurs fantome
     def enleverGhostUsers(self):
-        for cle,valeur in self.usersDictionnary.items():
-            self.duree = self.usersLogTime() - self.usersDictionnary.get[cle]
-            if self.duree.seconds > 43200:
-                del self.usersDictionnary[cle]
+        for cle,valeur in self.usersDictionnary.items():  #pour chaque combinaison du dictionnaire
+            self.duree = datetime.now() - self.usersDictionnary.get[cle]  #la durée entre le temps actuelle et l'heure de badgage de l'utilisateur
+            if self.duree.seconds > 43200:  #si cette durée est supérieur a 43200 secondes , soit 12heures
+                del self.usersDictionnary[cle] #on le supprime de la liste
