aba0ebf7126bab88b2ba480eeb041cbd31ee4369
JUNIER <christophejunier@gmail.com>
2017-03-16 21:09:28 +0100

    ajout de la class nbutilisateurs

---
+++ modele/ClassAfficherNbUsers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Fait par dany BITARD
+
+from datetime import datetime
+
+class afficherNbUsers:
+
+    def _init_(self):
+        self.usersDictionnary = {}
+        self.usersLogTime = datetime.now()
+
+    def GererUtilisateurListe(self,ID_badge):
+        if self.usersDictionnary.has_key(ID_badge):
+            del self.usersDictionnary[ID_badge]
+        else:
+            self.usersDictionnary[ID_badge] = self.usersLogTime
+
+    def afficherNbUsers(self,ID_badge):
+        return("Nombre de personne présente :", len(liste_entrer))#retourne le nombre de personne présente
+
+
+    def enleverGhostUsers(self):
+        for cle,valeur in self.usersDictionnary.items():
+            self.duree = self.usersLogTime() - self.usersDictionnary.get[cle]
+            if self.duree.seconds > 43200:
+                del self.usersDictionnary[cle]
